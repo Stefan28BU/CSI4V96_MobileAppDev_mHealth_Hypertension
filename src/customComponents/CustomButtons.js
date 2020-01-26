c2b656f7112653774a3c19fdd8256f8c2b9a2f58
Yufan Xu <yufanxu@YufandeMacBook-Pro.local>
2020-01-25 21:10:19 -0600

    Custom button

---
+++ src/customComponents/CustomButtons.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+
+export const WelcomeButton = (props) => {
+    const { title = {}, style = {}, textStyle = {}, onPress } = props;
+
+    return (
+        <TouchableOpacity title={title} onPress={onPress} style={[styles.welcomeBtnContainer, style]}>
+            <Text style={[styles.welcomeText, textStyle]}>{props.title}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    welcomeBtnContainer: {
+        display: 'flex',
+        height: 50,
+        width: 250,
+        margin: 12,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000000',
+        shadowColor: 'black',
+        shadowOpacity: 0.7,
+        shadowOffset: { height: 0, width: 0 },
+        shadowRadius: 8,
+    },
+
+    welcomeText: {
+        fontSize: 16,
+        color: '#FFFFFF',
+        textTransform: 'uppercase'
+    },
+});
--- src/pages/WelcomeScreen.js
+++ src/pages/WelcomeScreen.js
@@ -1,18 +1,43 @@
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { WelcomeButton } from '../customComponents/CustomButtons'
 
 export class WelcomeScreen extends Component {
     render() {
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Button title="Continue As Guest" onPress={() => this.props.navigation.navigate('Home')} />
-          <Button
-            title="Sign In" onPress={() => this.props.navigation.navigate('Login')}
-          />
-          <Button title="Sign Up As Researcher" onPress={() => alert('Nah you cannot sign up')} />
+          <View style={styles.welcomeScreenWrapper}>
+
+            <WelcomeButton title="Continue As Guest" onPress={() => this.props.navigation.navigate('Home')}/>
+          <WelcomeButton title="Sign In" onPress={() => this.props.navigation.navigate('Login')}/>
+          <WelcomeButton title="Sign Up As Researcher" onPress={() => alert('Nah you cannot sign up')} />
         </View>
       );
     }
   }
   
+  const styles = StyleSheet.create({
+      welcomeScreenWrapper: {
+        flex:1, 
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      welcomeScreenButtonContainer: {
+        display: 'flex',
+        height: 50,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        backgroundColor: '#2AC062',
+        shadowColor: '#2AC062',
+        shadowOpacity: 0.4,
+        shadowOffset: { height: 10, width: 0 },
+        shadowRadius: 20,
+      },
+      welcomeScreenText: {
+        fontSize: 16,
+        textTransform: 'uppercase',
+        color: 'white',
+      }
+  })
