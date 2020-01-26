671667c0e91d5d753ff0ec8ebec743b8e7bbf784
fubo130 <bo_fu2@baylor.edu>
2020-01-25 23:04:30 -0600

    Fixed sign up page error

--- src/pages/ExploreScreen.js
+++ src/pages/ExploreScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createAppContainer } from 'react-navigation';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export class ExploreScreen extends React.Component {
     render() {
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Button, View, StyleSheet, Text } from 'react-native';
 import { Form, TextValidator } from 'react-native-validator-form';
 
 export class Signup extends Component {
@@ -9,21 +9,22 @@ export class Signup extends Component {
             username: '',
             password: '',
             confirmPassword: '',
-            submit: false,
+            submitted: false,
         };
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
     
 
     componentWillMount() {
         Form.addValidationRule('passwordMatch', (value)=>{
-            if (value !== this.state.password) {
+            if (value === undefined || value !== this.state.password) {
                 this.setState({
-                    submit: false
+                    submitted: false
                 })
                 return false;
             }
             this.setState({
-                submit: true
+                submitted: true
             })
             return true;
         });
@@ -34,14 +35,15 @@ export class Signup extends Component {
     }
 
     handleSubmit() {
-        if (this.state.submit) {
+        
+        if (this.state.submitted) {
             Alert.alert('Credentials', `${this.state.username} + ${this.state.password}`);
         } else {
             Alert.alert(
-                'Alert Title',
-                'Message',
+                'Wrong information!',
+                'Please check your email address or password and try again!',
                 [
-                    {text: 'Invalid username or password!', onPress: () => console.log('Press button!')}
+                    {text: 'Retry', onPress: () => console.log('Press button!')}
                 ],
                 {cancelable: false}
             )
@@ -51,21 +53,27 @@ export class Signup extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <Text style = {styles.title}>Sign Up</Text>
                 <Form ref = "Signup" onSubmit = {this.handleSubmit}>
+                    <Text>Email: </Text>
                     <TextValidator
+                        title= "Email: "
                         style={styles.input}
                         name = "email"
                         lable = "Email"
                         validators = {['required', 'isEmail']}
                         errorMessages = {['This field is required!', 'Email invalid!']}
+                        onError={errors => this.setState({submitted: false})}
                         placeholder = "Email"
                         type = "text"
                         keyboardTypes = "email-address"
                         value = {this.state.username}
                         onChangeText={(username) => this.setState({ username })} 
                     />
+                    <Text>Password: </Text>
                     <TextValidator
-                        style={styles.input}
+                        title= "Password: "
+                        style={styles.input}    
                         name = "password"
                         lable = "Password"
                         validators = {['required']}
@@ -76,19 +84,21 @@ export class Signup extends Component {
                         onChangeText={(password) => this.setState({ password })}
                         secureTextEntry={true}
                     />
+                    <Text>Re-enter Password: </Text>
                     <TextValidator
+                        title= "Re-enter Password: "
                         style={styles.input}
                         name = "repeatPassword"
                         lable = "Confirm Password"
                         validators = {['required', 'passwordMatch']}
-                        errorMessages = {['This field is required!', 'The password does not match, please re-enter your pasword!']}
+                        errorMessages = {['This field is required!', 'The password does not match!']}
                         placeholder = "Password"
                         type = "text"
                         value = {this.state.confirmPassword}
                         onChangeText={(confirmPassword) => this.setState({ confirmPassword })}
                         secureTextEntry={true}
                     />
-                    <Button title = "Sign Up" onPress = {this.handleSubmit} />
+                    <Button style={styles.button} title = "Sign Up" onPress = {this.handleSubmit} />
                 </Form>
             </View>
         )
@@ -98,7 +108,6 @@ export class Signup extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      width: 400,
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
@@ -108,8 +117,18 @@ const styles = StyleSheet.create({
       width: 200,
       height: 44,
       padding: 10,
-      borderWidth: 1,
       borderColor: 'black',
       marginBottom: 10,
+      justifyContent: 'center',
+    },
+    title: {
+        alignContent: 'center', 
+        justifyContent: 'center', 
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    name: {
+        fontSize: 15,
+        marginTop: 25,
     },
   });
