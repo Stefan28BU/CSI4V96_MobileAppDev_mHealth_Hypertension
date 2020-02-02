56aacddf31c383cea0c5cbed3d8d94596fee2423
Haizhepi <yunzheliu1998@gmail.com>
2020-02-02 14:35:29 -0600

    backend lambda

---
+++ exapmle_code/index.js
@@ -0,0 +1,32 @@
+import Amplify from 'aws-amplify';
+import config from './config';
+import { Auth } from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+});
+
+registerUser = async () => {
+    const username = "yunzhe_liu@baylor.edu";
+    const email = "yunzhe_liu@baylor.edu";
+    const passowrd = "123456Aa!";
+    try {
+        const signUpResponse = await Auth.signUp({
+            username,
+            password,
+            attributes: {
+                email: email
+            }
+        });
+        console.log(signUpResponse);
+    }
+    catch(error) {
+        console.log(error);
+    }
+};
+
