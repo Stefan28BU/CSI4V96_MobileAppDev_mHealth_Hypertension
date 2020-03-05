b52a3e7e64fdd1a552793231512b26e188e194e2
Yufan Xu <yufanxu@YufandeMacBook-Pro.local>
2020-03-04 18:31:22 -0600

    merge

--- src/pages/SignUpScreen.js
+++ src/pages/SignUpScreen.js
@@ -16,8 +16,8 @@ export class SignUpScreen extends Component {
             submited: false,
             confirmKey: '',
         };
-        this.handleSubmit=this.handleSubmit.bind(this); 
-        this.handleConfirm=this.handleConfirm.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleConfirm = this.handleConfirm.bind(this);
     }
 
 
@@ -42,27 +42,27 @@ export class SignUpScreen extends Component {
 
     async handleSubmit() {
         console.log(this.state.checked);
-    
+
         if (this.state.checked && this.state.password === this.state.confirmPassword) {
             const username = this.state.username;
             const password = this.state.password;
             console.log(username + " " + password);
             try {
                 const signUpResponse = await Auth.signUp({
-                username, 
-                password,
-                attributes: {
-                    email: username
-                }
+                    username,
+                    password,
+                    attributes: {
+                        email: username
+                    }
                 });
                 console.log('not a error');
                 console.log(signUpResponse);
-                this.setState({submited: true});
+                this.setState({ submited: true });
 
                 // do some saving?
-            } catch(error) {
+            } catch (error) {
                 console.log(error);
-                this.setState({submited: false});
+                this.setState({ submited: false });
                 Alert.alert(error.message);
             }
         } else {
@@ -70,19 +70,21 @@ export class SignUpScreen extends Component {
                 'Wrong information!',
                 'Please check your email address or password and try again!',
                 [
-                    {text: 'Retry', onPress: () => {
-                        console.log('resending');
-                    }}
+                    {
+                        text: 'Retry', onPress: () => {
+                            console.log('resending');
+                        }
+                    }
                 ],
-                {cancelable: false}
+                { cancelable: false }
             )
         }
     }
 
     handleResendSignUp() {
         Auth.resendSignUp(this.state.username)
-        .then(() => console.log('successfully resend'))
-        .catch((err) => console.log(err));
+            .then(() => console.log('successfully resend'))
+            .catch((err) => console.log(err));
     }
 
     navigateToHome() {
@@ -93,98 +95,100 @@ export class SignUpScreen extends Component {
         // const go = this.props.navigation.navigate('Learn');
 
         Auth.confirmSignUp(this.state.username, this.state.confirmKey)
-        .then(() => {
-            
-            this.navigateToHome();
-            console.log('successful confirm signed up')
-        })
-        .catch(err => {
+            .then(() => {
 
-            console.log('error confirm signing up: ', err)
-            Alert.alert(
-                'Wrong confirm key!',
-                'Please check your confirm key and try again!',
-                [
-                    {text: 'Retry', onPress: () => {
-                        console.log('Press button!')
-                        this.handleResendSignUp();
-                    }}
-                ],
-                {cancelable: false}
-            )
-        });
+                this.navigateToHome();
+                console.log('successful confirm signed up')
+            })
+            .catch(err => {
+
+                console.log('error confirm signing up: ', err)
+                Alert.alert(
+                    'Wrong confirm key!',
+                    'Please check your confirm key and try again!',
+                    [
+                        {
+                            text: 'Retry', onPress: () => {
+                                console.log('Press button!')
+                                this.handleResendSignUp();
+                            }
+                        }
+                    ],
+                    { cancelable: false }
+                )
+            });
     }
 
-    render() {   
+    render() {
         return (
-            <KeyboardAvoidingView style = {styles.keyboardInput}  behavior="padding" enabled>
-                {this.state.submited === false?<Form ref = "Signup" onSubmit = {this.handleSubmit}>
-                        <Text style = {styles.title}>Sign Up</Text>
-                        <Text style = {styles.name}>Email: </Text>
-                        <TextValidator
-                            title= "Email: "
-                            style={styles.input}
-                            name = "email"
-                            lable = "Email"
-                            validators = {['required', 'isEmail']}
-                            errorMessages = {['This field is required!', 'Email invalid!']}
-                            onError={errors => this.setState({checked: false})}
-                            placeholder = "Email"
-                            type = "text"
-                            keyboardTypes = "email-address"
-                            value = {this.state.username}
-                            onChangeText={(username) => this.setState({ username })} 
-                        />
-                        <Text style = {styles.name}>Password: </Text>
-                        <TextValidator
-                            title= "Password: "
-                            style={styles.input}    
-                            name = "password"
-                            lable = "Password"
-                            validators = {['required']}
-                            errorMessages = {['This field is required!']}
-                            placeholder = "Password"
-                            type = "text"
-                            value = {this.state.password}
-                            onChangeText={(password) => this.setState({ password })}
-                            secureTextEntry={true}
-                        />
-                        <Text style = {styles.name}>Re-enter Password: </Text>
-                        <TextValidator
-                            title= "Re-enter Password: "
-                            style={styles.input}
-                            name = "repeatPassword"
-                            lable = "Confirm Password"
-                            validators = {['required']}
-                            errorMessages = {['This field is required!']}
-                            placeholder = "Password"
-                            type = "text"
-                            value = {this.state.confirmPassword}
-                            onChangeText={(confirmPassword) => this.setState({ confirmPassword })}
-                            secureTextEntry={true}
-                        />
-                    <Button style={styles.button} title = "Sign Up" onPress = {this.handleSubmit} />
+            <KeyboardAvoidingView style={styles.keyboardInput} behavior="padding" enabled>
+                {this.state.submited === false ? <Form ref="Signup" onSubmit={this.handleSubmit}>
+                    <Text style={styles.title}>Sign Up</Text>
+                    <Text style={styles.name}>Email: </Text>
+                    <TextValidator
+                        title="Email: "
+                        style={styles.input}
+                        name="email"
+                        lable="Email"
+                        validators={['required', 'isEmail']}
+                        errorMessages={['This field is required!', 'Email invalid!']}
+                        onError={errors => this.setState({ checked: false })}
+                        placeholder="Email"
+                        type="text"
+                        keyboardTypes="email-address"
+                        value={this.state.username}
+                        onChangeText={(username) => this.setState({ username })}
+                    />
+                    <Text style={styles.name}>Password: </Text>
+                    <TextValidator
+                        title="Password: "
+                        style={styles.input}
+                        name="password"
+                        lable="Password"
+                        validators={['required']}
+                        errorMessages={['This field is required!']}
+                        placeholder="Password"
+                        type="text"
+                        value={this.state.password}
+                        onChangeText={(password) => this.setState({ password })}
+                        secureTextEntry={true}
+                    />
+                    <Text style={styles.name}>Re-enter Password: </Text>
+                    <TextValidator
+                        title="Re-enter Password: "
+                        style={styles.input}
+                        name="repeatPassword"
+                        lable="Confirm Password"
+                        validators={['required']}
+                        errorMessages={['This field is required!']}
+                        placeholder="Password"
+                        type="text"
+                        value={this.state.confirmPassword}
+                        onChangeText={(confirmPassword) => this.setState({ confirmPassword })}
+                        secureTextEntry={true}
+                    />
+                    <Button style={styles.button} title="Sign Up" onPress={this.handleSubmit} />
                 </Form>
-                
-                :
-                
-                <Form ref = "Signup" onSubmit = {this.handleSubmit}>
-                        <Text style = {styles.title}>Confirmation</Text>
-                        <Text style = {styles.name}>Confirm Key: </Text>
+
+                    :
+
+                    <Form ref="Signup" onSubmit={this.handleSubmit}>
+                        <Text style={styles.title}>Confirmation</Text>
+                        <Text style={styles.name}>Confirm Key: </Text>
                         <TextValidator
-                            title= "Confirm Key: "
+                            title="Confirm Key: "
                             style={styles.input}
-                            name = "Confirm Key"
-                            lable = "Confirm Key"
-                            validators = {['required']}
-                            errorMessages = {['This field is required!']}
-                            placeholder = "Confirm key"
-                            type = "text"
-                            keyboardTypes = "text"
-                            value = {this.state.confirmKey}
-                            onChangeText={(confirmKey) => this.setState({ confirmKey })} 
+                            name="Confirm Key"
+                            lable="Confirm Key"
+                            validators={['required']}
+                            errorMessages={['This field is required!']}
+                            placeholder="Confirm key"
+                            type="text"
+                            keyboardTypes="text"
+                            value={this.state.confirmKey}
+                            onChangeText={(confirmKey) => this.setState({ confirmKey })}
                         />
-                        <Button style={styles.button} title = "Confirm" onPress = {this.handleConfirm} />
+                        <Button style={styles.button} title="Confirm" onPress={this.handleConfirm} />
                     </Form>
                 }
             </KeyboardAvoidingView>
@@ -200,16 +204,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     input: {
-      width: 200,
-      height: 44,
-      padding: 10,
-      borderColor: 'black',
-      marginBottom: 10,
-      justifyContent: 'center',
-      borderBottomWidth: 1,
+        width: 200,
+        height: 44,
+        padding: 10,
+        borderColor: 'black',
+        marginBottom: 10,
+        justifyContent: 'center',
+        borderBottomWidth: 1,
     },
     title: {
-        alignContent: 'center', 
+        alignContent: 'center',
         justifyContent: 'center',
         fontSize: 20,
         fontWeight: 'bold',
@@ -220,4 +224,4 @@ const styles = StyleSheet.create({
         fontSize: 15,
         marginTop: 25,
     },
-  });
+});
