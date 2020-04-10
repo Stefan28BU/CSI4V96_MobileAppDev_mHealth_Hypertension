2b3dbb0b48baac4ac4dee072ae2a7b5e9754885a
Xingan Wan <38301743+RoyXingan@users.noreply.github.com>
2020-04-09 19:20:38 -0500

    Local Notification Updates

--- src/pages/notification/Notifi.js
+++ src/pages/notification/Notifi.js
@@ -41,6 +41,12 @@ export default class AppContainer extends React.Component {
 
   componentDidMount() {
     this.registerForPushNotificationsAsync();
+
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
     this._notificationSubscription = Notifications.addListener(this._handleNotification);
   }
 
@@ -48,6 +54,27 @@ export default class AppContainer extends React.Component {
     Vibration.vibrate();
     console.log(notification);
     this.setState({ notification: notification });
+  };
+
+  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/dashboard/notifications
+  sendPushNotification = async () => {
+    const message = {
+      to: this.state.expoPushToken,
+      sound: 'default',
+      title: 'Original Title',
+      body: 'And here is the body!',
+      data: { data: 'goes here' },
+      _displayInForeground: true,
+    };
+    const response = await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
   };
 
   render() {
@@ -67,3 +94,10 @@ export default class AppContainer extends React.Component {
     );
   }
 }
+
+/*  TO GET PUSH RECEIPTS, RUN THE FOLLOWING COMMAND IN TERMINAL, WITH THE RECEIPTID SHOWN IN THE CONSOLE LOGS
+
+    curl -H "Content-Type: application/json" -X POST "https://exp.host/--/api/v2/push/getReceipts" -d '{
+      "ids": ["YOUR RECEIPTID STRING HERE"]
+      }'
+*/
